3e36aa55470fffae56e8df3a6e817f7b86684295
mio <marcoscolladosegura@ied.edu>
2020-09-29 15:30:13 +0200

    this commit deals with basic Routing of the site

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import React from 'react';
+//Deps
+import React,{Suspense} from 'react';
+//logo
 import logo from './logo.svg';
 import './App.css';
+//Paths
+import PATHS from './04-Constants/Routes'
+//Components
+import {BrowserRouter, Switch, Route, Redirect } from 'react-router-dom';
+
+
+//Pages
+const MainPage = React.lazy(()=>import('./01-Pages/MainPage'));
+const OutOfBounds = React.lazy(()=>import('./01-Pages/OutOfBounds'));
+
+
+
+const COMPONENT_PATHS = [
+  {Component:MainPage,path:PATHS.MAIN_PAGE},
+  {Component:OutOfBounds,path:PATHS.OUT_OF_BOUNDS},
+]
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+    <Switch>
+      {COMPONENT_PATHS.map(({path,Component})=>(
+        <Route path={path} exact key={path}>
+          <Suspense fallback={<div>loading...</div>}>
+            <Component/>
+          </Suspense>
+        </Route>
+      ))}
+        <Redirect to={PATHS.PAGE_NOT_FOUND}/>
+    </Switch>
+  </BrowserRouter>
   );
 }
 
