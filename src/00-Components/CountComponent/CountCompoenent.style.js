33e4be02e8b37555a2e225873b1f26735bf28782
mio <marcoscolladosegura@ied.edu>
2020-09-30 22:09:34 +0200

    this adds styles to the test component using palette

--- src/00-Components/CountComponent/CountCompoenent.style.js
+++ src/00-Components/CountComponent/CountCompoenent.style.js
@@ -1,15 +1,19 @@
 import styled from 'styled-components'
+import {blueColor,whiteColor} from '../../07-Styles/colorPalette';
 
 const StyledCount = styled.div`
     display:flex;
+    align-items:center;
     height:30px;
+    width:86px;
     p{
         margin:0 1em;
     }
     button{
         width:22px;
         height:22px;
-        background-color:red;
+        background-color:${blueColor};
+        color:${whiteColor};
         line-height:9px;
         border:none;
     }
